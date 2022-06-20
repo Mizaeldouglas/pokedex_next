33c6ba5d170438db6f2d0df5a83823cad5406956
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-06-19 22:47:12 -0300

    Projeto Finalizado

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import styles from "../../styles/NavBar.module.css"
+import styles from "../styles/NavBar.module.css"
 
 export default function NavBar(){
 	return(
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,51 @@
 import styles from "../styles/Home.module.css"
+import Image from "next/image";
+
+import Card from "../components/Card";
+
+export async function getStaticProps(){
+
+	const maxPokemons = 251;
+	const api = "https://pokeapi.co/api/v2/pokemon/"
+
+	const res = await fetch(`${api}/?limit=${maxPokemons}`)
+	const data = await res.json()
+
+	data.results.forEach((item,index) => {
+		item.id = index + 1
+	});
+	return{
+		props: {
+			pokemons: data.results,
+		
+		}
+	}
+
+}
 
 
-export default function Home() {
+export default function Home({pokemons}) {
   return (
-    <div>
-      <h1>Hello World!!</h1>
-    </div>
+	<>
+		<div className={styles.title_container}>
+			<h1 className={styles.title}>
+				Poke<span>Dex</span>
+			</h1>
+			<Image
+			src="/image/pokeball.png"
+			width="50"
+			height="50"
+			alt="PokeNext"
+			/>
+		</div>
+
+
+		<div className={styles.pokemon_container}>
+				{pokemons.map((pokemon) =>(
+					<Card key={pokemon.id} pokemon={pokemon} />
+				))}
+		</div>
+
+	</>
   )
 }
